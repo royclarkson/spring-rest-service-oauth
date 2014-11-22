32a5d78d3e149f51296cf7c995a0d1bd570adf5c
Roy Clarkson <rclarkson@pivotal.io>
2014-11-21 19:53:01 -0600

    Update initial list of users

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 insert into user(id, name, login, password) values (1,'Roy','roy','spring');
-insert into user(id, name, login, password) values (2,'SomeOne','so','spring');
+insert into user(id, name, login, password) values (2,'Craig','craig','spring');
+insert into user(id, name, login, password) values (3,'Greg','greg','spring');
