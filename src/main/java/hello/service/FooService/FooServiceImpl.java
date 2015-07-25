9637e4b3c2dc64c83b251f783389e1ad9d7bab10
Admir Sabanovic <admir.sabanovic@ibu.edu.ba>
2015-07-25 17:10:29 +0200

    postgresql, abstract shared model, shared service

--- src/main/java/hello/service/FooService/FooServiceImpl.java
+++ src/main/java/hello/service/FooService/FooServiceImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package hello.service.FooService;
 
 import hello.dao.service.SharedService;
@@ -6,14 +21,10 @@ import hello.data.Foo.FooRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by asabanovic on 7/25/15.
- */
 @Service
 public class FooServiceImpl extends SharedService<Foo, FooRepository> implements FooService {
     @Autowired
     FooRepository fooRepository;
-
     public void setRepository(){
         super.setRepository(fooRepository);
     }
