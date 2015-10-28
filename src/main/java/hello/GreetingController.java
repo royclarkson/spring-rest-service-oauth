62e6ac63bf579e676993d2ab67e9c261933b90ff
Dave Syer <dsyer@pivotal.io>
2015-10-28 11:03:28 +0000

    Update to Spring Boot 1.3

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -16,24 +16,25 @@
 
 package hello;
 
-import hello.data.User;
-
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hello.data.User;
 
 @RestController
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
-	
+
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/greeting")
 	public Greeting greeting(@AuthenticationPrincipal User user) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, user.getName()));
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, user.getName()));
 	}
 
 }
